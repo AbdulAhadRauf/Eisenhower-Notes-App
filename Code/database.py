1ba9f6e6d4ddd36b7fba21f828aefa2333488f79
Abdul Ahad Rauf <120673653+AbdulAhadRauf@users.noreply.github.com>
2025-07-24 18:15:17 +0530

    Add files via upload

--- Code/database.py
+++ Code/database.py
@@ -1,17 +1,15 @@
+# database.py
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 
-# Load environment variables from .env file
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+# url = "postgresql://postgres.hcimrfnwrxyilsebfcmu:[YOUR-PASSWORD]@aws-0-ap-south-1.pooler.supabase.com:6543/postgres"
+engine = create_engine(f"postgresql://{os.environ.get('DB_USER')}:{os.environ.get('DB_PASSWORD')}@{os.environ.get('DB_HOST')}:{os.environ.get('DB_PORT')}/{os.environ.get('DB_NAME')}")
 
-engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}  # for SQLite only # type: ignore
-)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
